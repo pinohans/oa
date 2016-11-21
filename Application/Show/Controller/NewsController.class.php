c5bd46b3f8e09d0e2ec9931f8bc40c0d37905889
Pinohans <492375757@qq.com>
2016-11-21 13:03:27 +0800

    测试版 V2.4
    1.增加json返回新闻，m=show&c=news&a=index&page=

---
+++ Application/Show/Controller/NewsController.class.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Show\Controller;
+use Think\Controller;
+class NewsController extends Controller {
+	public function index(){
+		$page=$_GET['page'];
+		$data=M('info')->where('is_public = 1 and is_del = 0')->order('update_time')->limit('%d,%d',$page*10,$page*10+10)->field('name,content,update_time,user_name')->select();
+		$this->ajaxReturn(json_encode($data));
+	}
+}
