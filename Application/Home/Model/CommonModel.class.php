127e5bebc6d075c1ea570a4f6359128113d22646
Pinohans <492375757@qq.com>
2016-11-22 19:37:08 +0800

    测试版 v3.1.6
    1.增加传参，增加排序，处理更新时间

--- Application/Home/Model/CommonModel.class.php
+++ Application/Home/Model/CommonModel.class.php
@@ -7,7 +7,7 @@ class CommonModel extends Model {
 	protected $_auto = array(
 		array('is_del', '0', self::MODEL_INSERT),
 		array('create_time', 'time', self::MODEL_INSERT, 'function'),
-		array('update_time', 'time', self::MODEL_UPDATE, 'function'),
+		array('update_time', 'time', self::MODEL_BOTH, 'function'),
 		array('user_id', 'get_user_id', self::MODEL_INSERT, 'callback'),
 		array('user_name', 'get_user_name', self::MODEL_INSERT, 'callback'),
 		array('dept_id', 'get_dept_id', self::MODEL_INSERT, 'callback'),
--- Application/Show/Controller/NewsController.class.php
+++ Application/Show/Controller/NewsController.class.php
@@ -5,7 +5,7 @@ use Think\Controller;
 class NewsController extends Controller {
 	public function index(){
 		$page=$_GET['page'];
-		$data=M('info as a,think_system_folder as b')->where('a.is_public = 1 and a.is_del = 0 and a.folder = b.id')->order('update_time')->limit('%d,10',$page*10)->field('a.id,a.name,a.update_time,a.user_name,b.name as type')->select();
+		$data=M('info as a,think_system_folder as b')->where('a.is_public = 1 and a.is_del = 0 and a.folder = b.id')->order('update_time desc')->limit('%d,10',$page*10)->field('a.id,a.name,a.create_time,a.update_time,a.user_name,b.name as type')->select();
 		echo json_encode($data);
 	}
 	public function read()
