54b0f431e86a74749833f4d788de945dbb5853d4
Pinohans <492375757@qq.com>
2016-11-23 14:24:15 +0800

    测试版v3.1.8
    1.修改分页

--- Application/Show/Controller/NewsController.class.php
+++ Application/Show/Controller/NewsController.class.php
@@ -4,8 +4,9 @@ namespace Show\Controller;
 use Think\Controller;
 class NewsController extends Controller {
 	public function index(){
-		$page=$_GET['page'];
-		$data=M('info as a,think_system_folder as b')->where('a.is_public = 1 and a.is_del = 0 and a.folder = b.id')->order('update_time desc')->limit('%d,10',$page*10)->field('a.id,a.name,a.create_time,a.update_time,a.user_name,b.name as type')->select();
+		var_dump($_GET['page']);
+		$page=intval(I('get.page'))*10;
+		$data=M('info as a,think_system_folder as b')->field('a.id,a.name,a.create_time,a.update_time,a.user_name,b.name as type')->where('a.is_public = 1 and a.is_del = 0 and a.folder = b.id')->order('update_time desc')->limit(sprintf("%d,10",$page))->select();
 		echo json_encode($data);
 	}
 	public function read()
