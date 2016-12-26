b289e66b9a298c758bd2762edd63bf50d379f552
Pinohans <492375757@qq.com>
2016-12-26 22:27:09 +0800

    v4.0.1
    1.修改一处误拼写

--- Application/Home/Model/GovViewModel.class.php
+++ Application/Home/Model/GovViewModel.class.php
@@ -6,7 +6,7 @@ use Think\Model\ViewModel;
 class  GovViewModel extends ViewModel {
 	public $viewFields=array(
 		'Gov'=>array('*'),
-		'GovLog'=>array('is_read','advice','comment','user_name'=>'recieve_user_name','user_id'=>'recieve_user_id','create_time'=>'recieve_create_time','update_time'=>'recieve_update_time','is_del'=>'recieve_is_del','_on'=>'gov.id=GovLog.gov_id')
+		'GovLog'=>array('is_read','advice','comment','user_name'=>'recieve_user_name','user_id'=>'recieve_user_id','create_time'=>'recieve_create_time','update_time'=>'recieve_update_time','is_del'=>'recieve_is_del','_on'=>'Gov.id=GovLog.gov_id')
 		);
 }
 ?>
